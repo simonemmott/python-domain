974731886dcf4b7642b8db744e8845dc567d59d2
simon <simon.emmott@yahoo.co.uk>
2018-12-25 23:52:16 +0000

    Added DomainConfig._populate_from_data

--- PythonDomain/src/k2_domain/__init__.py
+++ PythonDomain/src/k2_domain/__init__.py
@@ -1,8 +1,17 @@
 from utilities import strUtil
 
+
 class DomainConfig(object):
     def __init__(self, name, **kw):
-        self.name = strUtil.underscoreCase(name.lower())
-        self.title = kw.get('title', strUtil.titleCase(name))
-        self.data = kw.get('data', '${k2_HOME}/data/'+self.name)
+        if not kw.get('dict'):
+            self.name = strUtil.underscoreCase(name.lower())
+            self.title = kw.get('title', strUtil.titleCase(name))
+            self.data = kw.get('data', '${k2_HOME}/data/'+self.name)
+        else:
+            self._populate_from_data(kw.get('dict'))
+            
+    def _populate_from_data(self, data):
+        self.name = data.get('name')
+        self.title = data.get('title', strUtil.titleCase(self.name))
+        self.data = data.get('data', '${k2_HOME}/data/'+self.name)
         
